c0a7d31998bdfe9ff5671d2bce53a26713f33cf2
DESKTOP-T3KR0E0\Undefined <xx.bryan.xx@msn.com>
2018-10-01 11:02:31 -0500

    RAC FIXED

--- Oracle/WINDOWS/GAP CHECK POWERSHELL/gap_check.ps1
+++ Oracle/WINDOWS/GAP CHECK POWERSHELL/gap_check.ps1
@@ -17,7 +17,7 @@ _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
            |     [github.com/bryr0/]	   |
            +============================+
            |        gap_check.ps1 		|
-           |            v.1.11          |
+           |            v.1.12          |
            +----------------------------+
 #>
 
@@ -66,38 +66,44 @@ Function MAIL {
 
 Function GAP(){
 
-$T="`t"
-$_ = "_" * 73
+  $NC=0;
+  $T="`t"
+  $_ = "_" * 70
+  $BM  = "`n";
 
-$P="set serveroutput on;`n SET FEEDBACK OFF;`n DECLARE NS NUMBER(10); LS NUMBER(10); TIMED VARCHAR2(50); BEGIN FOR n IN( select cast( to_char( max( decode (archived, 'YES', sequence`#))) as varchar2(10)) sequence from v`$log group by thread`#) LOOP NS := n.sequence; select to_char(next_time,'DD-MON-YY:HH24:MI:SS') Time, sequence`# INTO TIMED, LS from v`$archived_log where sequence`# = ( NS ); dbms_output.put_line( TIMED || ' ' || LS); END LOOP; END; `n / `n exit;";
-$S="set serveroutput on;`n SET FEEDBACK OFF;`n DECLARE NS VARCHAR2(50); LS NUMBER(10); TIMED VARCHAR2(50); BEGIN FOR n IN( SELECT MAX(FIRST_TIME) Time FROM V`$LOG_HISTORY GROUP BY THREAD`#) LOOP NS := n.Time ; select to_char(max(FIRST_TIME),'DD-MON-YY:HH24:MI:SS') Time, max(sequence`#) sequence`# INTO TIMED, LS from v`$log_history where FIRST_TIME >=( NS); dbms_output.put_line( TIMED || ' ' || LS); END LOOP; END; `n / `n exit;"
+  $P="set serveroutput on;`n SET FEEDBACK OFF;`n DECLARE NS NUMBER(10); C NUMBER(10) := 0; LS NUMBER(10); TIMED VARCHAR2(50); BEGIN FOR n IN( select cast( to_char( max( decode (archived, 'YES', sequence`#))) as varchar2(10)) sequence from v`$log group by thread`#) LOOP NS := n.sequence; C := C + 1; select to_char(next_time,'DD-MON-YY:HH24:MI:SS') Time, sequence`# INTO TIMED, LS from v`$archived_log where sequence`# = ( NS ); dbms_output.put_line( TIMED || ' ' || LS || ' ' || C); END LOOP; END; `n / `n exit;";
+  $S="set serveroutput on;`n SET FEEDBACK OFF;`n DECLARE NS VARCHAR2(50); C NUMBER(10) := 0; LS NUMBER(10); TIMED VARCHAR2(50); BEGIN FOR n IN( SELECT MAX(FIRST_TIME) Time FROM V`$LOG_HISTORY GROUP BY THREAD`#) LOOP NS := n.Time ; select to_char(max(FIRST_TIME),'DD-MON-YY:HH24:MI:SS') Time, max(sequence`#) sequence`# INTO TIMED, LS from v`$log_history where FIRST_TIME >=( NS); dbms_output.put_line( TIMED || ' ' || LS || ' ' || C); END LOOP; END; `n / `n exit;"
 
-$QP = echo $P.replace("¦"," ") | sqlplus -S "system/$DBAPASS@primary"
-$QS = echo $S.replace("¦"," ") | sqlplus -S "sys/$DBAPASS@standby as sysdba"
+  $QP = echo $P.replace("¦"," ") | sqlplus -S "system/$DBAPASS@primary"
+  $QS = echo $S.replace("¦"," ") | sqlplus -S "sys/$DBAPASS@standby as sysdba"
 
-$PD=((echo $QP) -split " +")[0] # date
-$PG=((echo $QP) -split " +")[1] # gap
-$SD=((echo $QS) -split " +")[0] # date standby
-$SG=((echo $QS) -split " +")[1] # gap standby
+  $CO=((echo $QP) -split " +")[2] # counter
 
-$TOTAL=($PG-$SG)
+  For ($i=1; $i -le $CO; $i++) {
 
-$BM  = "`n" 
-$BM += " LOGS $T$T$T$T TIME $T$T$T SEQUENCE# `n" 
-$BM += " Last Generated on Primary $T $PD $T $PG `n" 
-$BM += " Last Applied on Standby $T $SD $T $SG `n" 
-$BM += " $_ `n"
-$BM += " GAP DIFERENCE$T$T$T$T$T$T $TOTAL `n"
-$BM += "`n"
+    $PD = ((echo $QP) -split " +")[$NC] # date
+    $PG = ((echo $QP) -split " +")[$NC+1] # gap
+    $SD = ((echo $QS) -split " +")[$NC] # date
+    $SG = ((echo $QS) -split " +")[$NC+1] # gap
 
-if ($TOTAL -ge $GA) {
-	foreach ($TO_ in $TO_USER) {
-		MAIL  -s "GAP ALERT" -c $BM -to $TO_;
-	}
-Exit
-  
-}
-return $BM
+    $TOTAL=($PG-$SG)
+    $BM += " LOGS $T$T$T$T TIME $T$T$T SEQUENCE# `n" 
+    $BM += " Last Generated on Primary[$i] $T $PD $T $PG `n" 
+    $BM += " Last Applied on Standby $T $SD $T $SG `n" 
+    $BM += " $_ `n"
+    $BM += " GAP DIFERENCE$T$T$T$T$T$T $TOTAL `n"
+    $BM += "`n"
+
+    $NC+=3
+  }
+
+  if ($TOTAL -ge $GA) {
+  	foreach ($TO_ in $TO_USER) {
+  		MAIL  -s "GAP ALERT" -c $BM -to $TO_;
+  	}
+  }
+
+ return $BM
 }
 
 $BM=GAP;
